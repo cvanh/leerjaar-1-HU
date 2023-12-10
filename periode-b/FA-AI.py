6b59e0170e92e58eaaf2f3ca6bfb6609bfaff086
constantijn van hartesveldt <constantijn@vanhartesveldt.nl>
2023-12-10 15:19:01 +0100

    init

---
+++ periode-b/FA-AI.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Oriëntatie op AI
+
+Final assignment: algoritmiek
+
+(c) 2019 Hogeschool Utrecht,
+Tijmen Muller (tijmen.muller@hu.nl)
+
+Opdracht:
+Beantwoord onderstaande vragen en werk onderstaande functies uit.
+Voeg commentaar toe om je code toe te lichten.
+
+Je kunt je functies testen met het gegeven raamwerk door het bestand
+uit te voeren (of met behulp van `pytest`, als je weet hoe dat werkt).
+Lever je werk in op Canvas als alle tests slagen.
+
+Let op! Het is niet toegestaan om bestaande modules te importeren en te
+        gebruiken, zoals `math` en `statistics`.
+"""
+
+naam = "Constantijn van Hartesveldt, 10"
+klas = "v1b"
+studentnummer = 1848569
+
+"""
+1.  Sorteeralgoritme
+
+    Hieronder staat de pseudocode van een sorteeralgoritme:
+    1. Startend vanaf het begin van een lijst, vergelijk elk element met zijn volgende buur.
+    2. Als het element groter is dan zijn volgende buur, verwissel ze van plaats.
+    3. Doorloop zo de lijst tot het eind.
+    4. Als er verwisselingen zijn geweest bij stap 2., ga naar stap 1.
+
+    1a. Handmatig toepassen
+        Gegeven is de lijst l = [ 4, 3, 1, 2 ]. Geef de waardes die deze
+        lijst aanneemt bij álle tussenstappen bij toepassing van
+        bovenstaand sorteeralgoritme.
+
+        we gebruiken bubblesort
+        begin: [4,3,1,2]
+        wissel [0] en [1] [3,4,1,2]
+        wissel [1] en [2] [3,1,4,2]
+        wissel [2] en [3] [3,1,2,4] [3] gaat niet meer schuiven
+        wissel [0] en [1] [1,3,2,4]
+        wissel [1] en [2] [1,2,3,4] 
+        vergelijk [0] en [1] [1,3,2,4] hetzelfde dus geen wissel
+"""
+#       TODO: [geef hier je antwoord]
+"""
+
+    1b. Implementatie
+        Implementeer het sorteeralgoritme in Python in een functie
+        hieronder genaamd my_sort(lst).
+
+    1c. Best en worst case
+        -   Stel je hebt een lijst met de waarden 1, 2 en 3. Bij welke
+            volgorde van de waarden in de lijst is het sorteeralgoritme
+            het snelste klaar (best-case scenario)?
+            Hoeveel vergelijkingen (zoals beschreven in stap 1. van de
+            pseudocode) zijn nodig geweest?
+
+            bubblesort: het is on2 dus 6 stappen minimaal
+            
+"""
+#           TODO: [geef hier je antwoord]
+"""
+
+
+        -   Bij welke volgorde van de waarden in de lijst is het
+            sorteeralgoritme het minst snel klaar (worst-case scenario)?
+            Hoeveel vergelijkingen zijn nodig geweest?
+
+            bubblesort: [4,3,2,1]
+
+"""
+#           TODO: [geef hier je antwoord]
+"""
+
+
+        -   Stel je hebt een lijst met de waarden 1 tot en met 4.
+            Wat is nu het best-case scenario?
+            Hoeveel vergelijkingen zijn er nodig?
+            En wat is nu het worst-case scenario?
+            Hoeveel vergelijkingen zijn er nodig?
+
+            bubblesort: [1,2,3,4]
+            er zijn evenveel vergelijkingen nodig maar er wordt niks aangepast
+"""
+#           TODO: [geef hier je antwoord]
+"""
+
+
+        -   (Optioneel) Stel je hebt een lijst met de waarden 1 tot en met n
+            (je weet nu dus niet precies hoeveel waarden er in de lijst
+            zitten, het zijn er 'n').
+            Wat is nu het best-case scenario?
+            Hoeveel vergelijkingen zijn er nodig?
+            En wat is nu het worst-case scenario?
+            Hoeveel vergelijkingen zijn er nodig?
+
+"""
+#           TODO: [geef hier je antwoord]
+"""
+"""
+
+
+def my_sort(lst):
+    """
+    Sorteer gegeven lijst volgens het algoritme zoals beschreven in de pseudocode.
+
+    1. Startend vanaf het begin van een lijst, vergelijk elk element met zijn volgende buur.
+    2. Als het element groter is dan zijn volgende buur, verwissel ze van plaats.
+    3. Doorloop zo de lijst tot het eind.
+    4. Als er verwisselingen zijn geweest bij stap 2., ga naar stap 1.
+
+    Zorg dat de gegeven lijst niet verandert, maar geef een nieuwe, gesorteerde variant van de lijst terug.
+
+    Args:
+        lst (list): Een lijst met elementen van gelijk type, bijvoorbeeld gehele getallen.
+
+    Returns:
+        list: Een nieuwe, gesorteerde variant van lijst `lst`.
+    """
+    # copy without pounter
+    lst_sorted = lst[:]
+
+    # bubblesort
+    for iter_num in range(len(lst_sorted)-1,0,-1):
+      for g in range(iter_num):
+         if lst_sorted[g]>lst_sorted[g+1]:
+            tmp = lst_sorted[g]
+            lst_sorted[g] = lst_sorted[g+1]
+            lst_sorted[g+1] = tmp
+
+    return lst_sorted
+
+
+def linear_search_recursive(lst, target):
+    """
+    Zoek een element in de gegeven lijst door middel van recursief lineair zoeken.
+
+    Zorg dat de inhoud van de gegeven lijst niet verandert.
+
+    Args:
+        lst (list):     Een lijst met elementen van gelijk type, bijvoorbeeld gehele getallen.
+        target (int):   Het gezochte element.
+
+    Returns:
+        bool: Of het element in de lijst voorkomt.
+    """
+    #https://nl.wikipedia.org/wiki/Lineair_zoeken https://stackoverflow.com/a/38311392/15070611
+
+    def linear_search(arr, size, key):
+        if (size == 0):
+            return False
+
+        if (arr[size - 1] == key):
+            return True 
+
+        return linear_search(arr, size - 1, key)
+
+    return linear_search(lst,len(lst),target)
+
+
+
+"""
+==========================[ HU TESTRAAMWERK ]================================
+Onderstaand staan de tests voor je code -- hieronder mag je niets wijzigen!
+Je kunt je code testen door deze file te runnen of met behulp van pytest.
+"""
+import random
+
+
+def test_id():
+    assert naam != "", "Je moet je naam nog invullen!"
+    assert studentnummer != -1, "Je moet je studentnummer nog invullen!"
+    assert klas != "", "Je moet je klas nog invullen!"
+
+
+def test_my_sort():
+    lst_test = random.choices(range(-99, 100), k=6)
+    lst_copy = lst_test.copy()
+    lst_output = my_sort(lst_test)
+
+    assert lst_copy == lst_test, "Fout: my_sort(lst) verandert de inhoud van lijst lst"
+    assert lst_output == sorted(lst_test), \
+        f"Fout: my_sort({lst_test}) geeft {lst_output} in plaats van {sorted(lst_test)}"
+
+
+def test_linear_search_recursive():
+    for _ in range(10):
+        lst_test = random.sample(range(20), 6)
+        target = random.randrange(20)
+        found = target in lst_test
+        lst_copy = lst_test.copy()
+
+        outcome = linear_search_recursive(lst_test, target)
+        assert lst_copy == lst_test, "Fout: linear_search_recursive(lst, target) verandert de inhoud van lijst lst"
+        assert outcome == found, \
+            f"Fout: linear_search_recursive({lst_test}, {target}) geeft {outcome} in plaats van {found}"
+
+
+def __main():
+    """ Test alle functies. """
+    # Noodzakelijk voor gekleurde tekst binnen een Windows terminal
+    import os
+    os.system("")
+
+    try:
+        print("\x1b[32m")   # Groene tekstkleur
+        test_id()
+
+        test_my_sort()
+        print("Je functie my_sort() werkt goed!")
+
+        test_linear_search_recursive()
+        print("Je functie linear_search_recursive() werkt goed!")
+
+        print("\nGefeliciteerd, alles lijkt te werken!")
+        print("Lever je werk nu in op Canvas...")
+
+    except AssertionError as ae:
+        print("\x1b[31m")   # Rode tekstkleur
+        if not ae:
+            print("Je code veroorzaakt onderstaande AssertionError:")
+            raise ae
+        else:
+            print(ae)
+
+    print("\x1b[0m")    # Reset tekstkleur
+
+
+if __name__ == '__main__':
+    __main()
+    
